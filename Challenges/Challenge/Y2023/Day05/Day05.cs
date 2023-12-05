7d280ab3b5f02a71dd67fa8bf86cacc2028cc997
Jack Maiden <jack.maiden@koder.ly>
2023-12-05 23:31:50 +0000

    Clean-up Day 5

--- Challenges/Challenge/Y2023/Day05/Day05.cs
+++ Challenges/Challenge/Y2023/Day05/Day05.cs
@@ -1,100 +1,87 @@
-﻿using System.Text.RegularExpressions;
+﻿using static System.Text.RegularExpressions.Regex;
 
 namespace Challenges.Challenge.Y2023.Day05;
 
 [ChallengeName("Day 5: If You Give A Seed A Fertilizer")]
 public class Day05: IChallenge
 {
-    public async Task<object> TaskPartOne(string input)
-    {
-        return almanacParse(input, longs => longs.Select(i => new Range(i, i)));
-    }
+    public async Task<object> TaskPartOne(string input) => Day5Solver(input, longs => longs.Select(i => new Range(i, i)));
+    
 
-    public async Task<object> TaskPartTwo(string input)
-    {
-        return almanacParse(input, longs => longs.Chunk(2).Select(i => new Range(i[0], i[0] + i[1] - 1))); ;
-    }
+    public async Task<object> TaskPartTwo(string input) => Day5Solver(input, longs => longs.Chunk(2).Select(i => new Range(i[0], i[0] + i[1] - 1)));
 
-    long almanacParse(string input, Func<long[], IEnumerable<Range>> parseRangesFunc)
+    private long Day5Solver(string input, Func<long[], IEnumerable<Range>> parseRangesFunc)
     {
-        IEnumerable<Range> longsToSearch;
-
         var inputBlocks = input.Split("\r\n\r\n").ToArray();
 
-        longsToSearch = parseRangesFunc(parseSeeds(inputBlocks[0]));
+        IEnumerable<Range> longsToSearch = parseRangesFunc(ParseSeeds(inputBlocks[0]));
 
-        var map = inputBlocks.Skip(1).Select(parseMaps).ToArray();
+        var map = inputBlocks.Skip(1).Select(ParseMaps).ToArray();
 
-        foreach (var search in map)
-        {
-            longsToSearch = longsToSearch.SelectMany(range => findRanges(range, search)).ToArray();
-        }
+        longsToSearch = map.Aggregate(longsToSearch, (current, search) => current.SelectMany(range => FindRanges(range, search)).ToArray());
 
-        return longsToSearch.Min(r => r.start);
+        return longsToSearch.Min(r => r.Start);
     }
 
+    private long[] ParseSeeds(string input) =>
+        Matches(input, @"\d+").Select(match => long.Parse(match.Value)).ToArray();
 
-    long[] parseSeeds(string input)
-    {
-        return (from match in Regex.Matches(input, @"\d+")
-            select long.Parse(match.Value)).ToArray();
-    }
-
-    Map[] parseMaps(string input)
+    private Map[] ParseMaps(string input)
     {
 
         var mapBodies = input.GetLines().Skip(1);
 
         return mapBodies
-            .Select(s =>
-            {
-                var x = s.Split(" ").Select(long.Parse).ToArray();
-
-                var src = new Range(x[1], x[1] + x[2] - 1);
-                var dest = new Range(x[0], x[0] + x[2] - 1);
-                return new Map(src, dest);
-            }).ToArray();
+            .Select(GenerateMap).ToArray();
     }
 
-    IEnumerable<Range> findRanges(Range range, Map[] map)
+    private Map GenerateMap(string? input)
+    {
+        var mapRangeArray = input.Split(" ").Select(long.Parse).ToArray();
+
+        var src = new Range(mapRangeArray[1], mapRangeArray[1] + mapRangeArray[2] - 1);
+        var dest = new Range(mapRangeArray[0], mapRangeArray[0] + mapRangeArray[2] - 1);
+
+        return new Map(src, dest);
+    }
+
+    private IEnumerable<Range> FindRanges(Range range, Map[] map)
     {
         var queue = new Queue<Range>();
         queue.Enqueue(range);
         while (queue.Any())
         {
             range = queue.Dequeue();
-            bool found = false;
+            var found = false;
             foreach (var lookup in map)
             {
-                if (lookup.src.start <= range.start && range.end <= lookup.src.end)
+                if (lookup.Src.Start <= range.Start && range.End <= lookup.Src.End)
                 {
-                    var offset = lookup.dest.start - lookup.src.start; //get the offset of the value
+                    var offset = lookup.Dest.Start - lookup.Src.Start; // get the offset of the value
 
-                    yield return new Range(range.start + offset, range.end + offset);
+                    yield return new Range(range.Start + offset, range.End + offset);
                     found = true;
                 }
-                else if(range.start < lookup.src.start && lookup.src.start <= range.end)
+                else if(range.Start < lookup.Src.Start && lookup.Src.Start <= range.End)
                 {
-                     queue.Enqueue(new Range(range.start, lookup.src.start - 1));
-                     queue.Enqueue(new Range(lookup.src.start, range.end));
+                     queue.Enqueue(new Range(range.Start, lookup.Src.Start - 1));
+                     queue.Enqueue(new Range(lookup.Src.Start, range.End));
                     found = true;
                 }
-                if (range.start < lookup.src.end && lookup.src.end <= range.end)
+                else if (range.Start < lookup.Src.End && lookup.Src.End <= range.End)
                 {
-                    queue.Enqueue(new Range(range.start, lookup.src.end - 1));
-                    queue.Enqueue(new Range(lookup.src.end, range.end));
+                    queue.Enqueue(new Range(range.Start, lookup.Src.End - 1));
+                    queue.Enqueue(new Range(lookup.Src.End, range.End));
                     found = true;
                 }
-
             }
-
             if (!found)
-                yield return new Range(range.start, range.end);
+                yield return new Range(range.Start, range.End);
         }
     }
 
-    record Range(long start, long end);
+    private record Range(long Start, long End);
 
-    record Map(Range src, Range dest);
+    private record Map(Range Src, Range Dest);
 
 }
