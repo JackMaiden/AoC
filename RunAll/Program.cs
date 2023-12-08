e79879b9c4fc9cf71d9736af2b2a605adbdc96b6
Jack Maiden <jack.maiden@koder.ly>
2023-12-08 10:08:44 +0000

    cleanup

--- RunAll/Program.cs
+++ RunAll/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Challenges;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 var execPath = AppDomain.CurrentDomain.BaseDirectory;
 
 var path = Path.Combine(execPath, @"..\..\..\");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
 
 var _runChallenges = new RunChallenges(path);
 
@@ -12,3 +16,9 @@ var result = await _runChallenges.RunAoCTask(new DateTime(2021, 12, 01), new Dat
 string json = JsonConvert.SerializeObject(result, Formatting.Indented);
 
 Console.WriteLine(json);
+
+stopWatch.Stop();
+
+var duration = stopWatch.Elapsed;
+
+Console.WriteLine($"Total Duration: {duration.TotalMilliseconds:N0}ms");
