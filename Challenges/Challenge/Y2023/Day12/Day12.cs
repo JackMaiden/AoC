70b4514b7aeb6c8ab7e78371a4da9467fae92341
Jack Maiden <jack.maiden@koder.ly>
2023-12-12 18:02:52 +0000

    Day 12 start

---
+++ Challenges/Challenge/Y2023/Day12/Day12.cs
@@ -0,0 +1,43 @@
+﻿namespace Challenges.Challenge.Y2023.Day12;
+
+[ChallengeName("")]
+public class Day12 : IChallenge
+{
+    public async Task<object> TaskPartOne(string input) => null;
+
+    public async Task<object> TaskPartTwo(string input) => null;
+
+    private int PossibleArrangements(string input)
+    {
+        var hotSprings = Parser(input);
+
+        Dictionary<HotSpring, int> usage = [];
+        foreach (var hotSpring in hotSprings)
+        {
+            Dictionary<int, int[]> possibilities = [];
+
+            for (int i = 0; i < hotSpring.Records.Count(); i++)
+            {
+                List<int> pos = [];
+
+                for (int j = 0; j < hotSpring.Conditions.Length; j++)
+                {
+                    
+                }
+
+
+                possibilities.Add(i, pos.ToArray());
+            }
+        }
+    }
+
+
+    private IEnumerable<HotSpring> Parser(string input) =>
+        input.GetLines().Select(s =>
+        {
+            var x = s.Split(" ");
+            return new HotSpring(x[0], x[1].GetIntArr());
+        });
+
+    private record HotSpring(string Conditions, IEnumerable<int> Records);
+}
