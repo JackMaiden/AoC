d598e95eaf15e56b713882d212cb4cbe10beaf9d
Jack Maiden <jack@jackmaiden.dev>
2024-11-17 13:29:25 +0000

    Add new this year option
    I could design a better way for project can run but this will do for now

---
+++ RunThisYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using Challenges;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+var execPath = AppDomain.CurrentDomain.BaseDirectory;
+
+var path = Path.Combine(execPath, @"..\..\..\");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var runChallenges = new RunChallenges(path);
+
+var result = await runChallenges.RunAoCTask(new DateTime(DateTime.Today.Year, 12, 01), new DateTime(DateTime.Today.Year, 12, 25));
+
+var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+Console.WriteLine(json);
+
+stopWatch.Stop();
+
+var duration = stopWatch.Elapsed;
+
+Console.WriteLine($"Total Duration: {duration.TotalMilliseconds:N0}ms");
