fa8719446c3a6c4cbe340dfa771c5e079a45f059
Jack Maiden <jack@jackmaiden.dev>
2024-11-29 16:48:21 +0000

    Day 2 (2015 edition)

--- Challenges/Challenge/Y2015/Day02/Day02.cs
+++ Challenges/Challenge/Y2015/Day02/Day02.cs
@@ -1,11 +1,31 @@
 ﻿namespace Challenges.Challenge.Y2015.Day02;
 
-[ChallengeName("")]
+[ChallengeName("Day 2: I Was Told There Would Be No Math")]
 public class Day02 : IChallenge
 {
-    public async Task<object?> TaskPartOne(string input) => null;
+    public async Task<object?> TaskPartOne(string input) => await Day02Part1(input.GetLines());
 
     public async Task<object?> TaskPartTwo(string input) => null;
 
+    private record struct Box(int L, int W, int H)
+    {
+        public int W { get; set; } = W;
+        public int L { get; set; } = L;
+        public int H { get; set; } = H;
+        public readonly IReadOnlyCollection<int> Areas = [L*W, W*H, H*L];
+        public int Paper => Areas.Sum() * 2 + Areas.Min();
+    }
+
+    private async Task<int> Day02Part1(IEnumerable<string> input)
+    {
+        var boxes = input.Select(s =>
+        {
+            var splits = s.Split("x");
+            return new Box(int.Parse(splits[0]), int.Parse(splits[1]), int.Parse(splits[2]));
+        });
+        
+        return boxes.Sum(s => s.Paper);
+    }
     
+
 }
