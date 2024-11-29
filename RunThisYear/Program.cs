523d1271c5fcf349a42eb88c6f7599464b442c69
Jack Maiden <jack@jackmaiden.dev>
2024-11-29 18:09:48 +0000

    Clean JSON Output

--- RunThisYear/Program.cs
+++ RunThisYear/Program.cs
@@ -1,4 +1,4 @@
-﻿using Challenges;
+using Challenges;
 using Newtonsoft.Json;
 using System.Diagnostics;
 
@@ -13,7 +13,8 @@ var runChallenges = new RunChallenges(path);
 
 var result = await runChallenges.RunAoCTask(new DateTime(DateTime.Today.Year, 12, 01), new DateTime(DateTime.Today.Year, 12, 25));
 
-var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+var json = JsonConvert.SerializeObject(result,
+    new JsonSerializerSettings { Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore });
 
 Console.WriteLine(json);
 
