5dd71555a4f8fe5ba3254320cbba9d16c42ba8c9
Jack Maiden <jack@jackmaiden.dev>
2024-12-01 18:12:42 +0000

    Day 1 2024

---
+++ Challenges/Challenge/Y2024/Day01/Day01.cs
@@ -0,0 +1,20 @@
+﻿namespace Challenges.Challenge.Y2024.Day01;
+
+[ChallengeName("Day 1: Historian Hysteria")]
+public class Day01 : IChallenge
+{
+    public async Task<object?> TaskPartOne(string input) => ParseInput(input, 0).Zip(ParseInput(input, 1))
+        .Select(p => Math.Abs(p.First - p.Second)).Sum();
+
+    public async Task<object?> TaskPartTwo(string input)
+    {
+        var appearances = ParseInput(input, 1).CountBy(x=> x).ToDictionary();
+        return ParseInput(input, 0).Select(i =>  i * appearances.GetValueOrDefault(i)).Sum();
+    }
+
+    private IEnumerable<int> ParseInput(string input, int column)
+    {
+        var x = input.GetLines().Select(l => l.GetIntArr());
+        return x.Select(s => s[column]).OrderBy(i => i);
+    }
+}
