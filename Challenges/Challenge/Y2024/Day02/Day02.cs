683006ce594f25602b032afd3452c0f5413b6a7f
Jack Maiden <jack@jackmaiden.dev>
2024-12-05 10:49:24 +0000

    Day 2

---
+++ Challenges/Challenge/Y2024/Day02/Day02.cs
@@ -0,0 +1,32 @@
+﻿namespace Challenges.Challenge.Y2024.Day02;
+
+[ChallengeName("Day 2: Red-Nosed Reports")]
+public class Day02 : IChallenge
+{
+    public async Task<object?> TaskPartOne(string input) => 
+        input.GetLines().Select(s => s.GetIntArr().ToList()).Count(IsSafe);
+
+    public async Task<object?> TaskPartTwo(string input) => input.GetLines().Select(s => s.GetIntArr().ToList()).Count(IsSafeDampener);
+
+    private bool IsSafeDampener(List<int> row)
+    {
+        if(IsSafe(row))
+            return true;
+
+        for (int i = 0; i < row.Count; i++)
+        {
+            var rowCopy = row.ToList();
+            rowCopy.RemoveAt(i);
+            
+            if(IsSafe(rowCopy))
+                return true;
+        }
+        return false;
+    }
+
+    private bool IsSafe(List<int> row)
+    {
+        var check = row.Zip(row.Skip(1), (a, b) => a - b).ToList();
+        return check.All(v => Math.Abs(v) <= 3) && (check.All(v => v > 0) || check.All(v => v < 0));
+    }
+}
